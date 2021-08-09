594e07f5ac5dc460ad6578bc3d796eb575ca22af
DenislavVelichkov <denislav.velichkov@gmail.com>
2021-08-09 11:59:17 +0300

    Initial commit.

---
+++ coding-solutions/src/Ch_01_Arrays_and_Strings/IsUnique_01/Solution.java
@@ -0,0 +1,7 @@
+package Ch_01_Arrays_and_Strings.IsUnique_01;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
--- com.interview.prep/src/com/interview/prep/solutions/Ch_01_DataStructures/App.java
+++
@@ -1,7 +0,0 @@
-package com.interview.prep.solutions.Ch_01_DataStructures;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
